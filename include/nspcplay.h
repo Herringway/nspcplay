95278bf65f9e76273a1021d9772439d20ae7e5e2
Cameron Ross <elpenguino@gmail.com>
2022-12-18 16:57:01 -0400

    fix c header

--- include/nspcplay.h
+++ include/nspcplay.h
@@ -1,13 +1,14 @@
 #include <stdint.h>
 
 struct NSPCPlayer;
+struct Song;
 
 struct NSPCPlayer* nspcplayNew(int32_t sampleRate);
 void nspcplayDestroy(struct NSPCPlayer* player);
 size_t nspcplayFillBuffer(struct NSPCPlayer* player, int16_t buffer[][2], size_t bufferLength);
-int32_t nspcplayLoadSequencePack(struct NSPCPlayer* player, const uint8_t* const data, size_t dataLength);
-int32_t nspcplayLoadInstrumentPack(struct NSPCPlayer* player, uint8_t* buffer, uint8_t const * const data, size_t dataLength);
-int32_t nspcplayInitializeInstruments(struct NSPCPlayer* player, uint8_t const * const buffer, uint16_t instrumentBase, uint16_t sampleBase);
+int32_t nspcplayLoadSequencePack(struct Song* player, const uint8_t* const data, size_t dataLength);
+int32_t nspcplayLoadInstrumentPack(struct Song* player, uint8_t* buffer, uint8_t const * const data, size_t dataLength);
+int32_t nspcplayInitializeInstruments(struct Song* player, uint8_t const * const buffer, uint16_t instrumentBase, uint16_t sampleBase);
 int32_t nspcplayLoadNSPCFile(struct NSPCPlayer* player, uint8_t const * const data, size_t dataLength);
 void nspcplayPlay(struct NSPCPlayer* player);
 void nspcplayStop(struct NSPCPlayer* player);
